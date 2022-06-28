f56baf101f0b519854e78ef017059630725a34d5
Victor Ferreira <victorferreira089@gmail.com>
2022-06-28 20:54:19 -0300

    Atualizacao para GUI

--- Gerador_senhas.py
+++ Gerador_senhas.py
@@ -1,5 +1,25 @@
-""" Gerador de senhas """
+"""
+Gerador de senhas em janela gráfica
+
+"""
 from random import choice
+import PySimpleGUI as sg
+from pyperclip import copy
+
+sg.theme('Darkblue3')
+
+layout = [
+    [sg.Text('Quantidade de caracteres: '), sg.Push(),
+     sg.Combo([i for i in range(4, 21)], readonly=True, default_value=8, key='-QUANTIDADE-')],
+    [sg.Text('Deseja caracteres especiais: '), sg.Push(), sg.Radio('Sim', '1', default=False, key='-SIM1-'),
+     sg.Radio('Não', '1', default=True, key='-NAO1-')],
+    [sg.Text('Deseja números na senha: '), sg.Push(), sg.Radio('Sim', '2', default=False, key='-SIM2-'),
+     sg.Radio('Não', '2', default=True, key='-NAO2-')],
+    [sg.Push(), sg.Button('Gerar Senha', key='-GERAR-'), sg.Push()],
+    [sg.Text('Sua senha é: '), sg.Text('', key='-OUTPUT-'), sg.Push(), sg.Button('Copiar senha', key='-COPIAR-')]
+]
+
+window = sg.Window('Gerador de senhas', layout, font=('Arial, 12'), size=(500,200))
 
 letras = 'abcdefghijklmnopqrstuvwxyz'
 LETRAS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -7,49 +27,44 @@ especiais = """`~!@#$%^&*()-_=+/*[{]}|\:;"'<,>.?"""
 numeros = '0123456789'
 
 possibilidades = [letras, LETRAS]
-senha_lista = []
-
-print('-='*20)
-print(f'{"Gerador de senhas":^40}')
-print('-='*20)
-
 while True:
-    try:
-        qtd = int(input('Digite a quantidade de caracteres que você quer para a senha: \n'))
-        if qtd > 0:
-            break
-        else:
-            print('Resposta inválida. Digite números maiores que zero.')
-    except:
-        print('Resposta inválida. Digite apenas números.')
+    event, values = window.read()
 
-while True:
-    spc = input('Deseja caracteres especiais na senha? [1-Sim] [2-Não]\n')
-    if spc.lower() in ['sim', '1']:
-        possibilidades.append(especiais)
+    if event == sg.WIN_CLOSED:
         break
-    elif spc.lower() in ['não', 'nao', '2']:
-        spc = None
-        break
-    else:
-        print('Resposta inválida.')
 
-while True:
-    num = input('Deseja números na senha? [1-Sim] [2-Não]\n')
-    if num.lower() in ['sim', '1']:
-        possibilidades.append(numeros)
-        break
-    elif num.lower() in ['não', 'nao', '2']:
-        num = None
-        break
-    else:
-        print('Resposta inválida.')
+    possibilidades = [letras, LETRAS]
+    senha_lista = []
 
-for q in range(qtd):
-    tipo = choice(possibilidades)
-    caracter = choice(tipo)
-    senha_lista.append(caracter)
+    if event == '-GERAR-':
 
-senha_final = ''.join(senha_lista)
+        if values['-QUANTIDADE-'] is not None:
+            qtd = values['-QUANTIDADE-']
 
-print(f'Sua senha é: \n{senha_final}')
+        if values['-SIM1-']:
+            possibilidades.append(especiais)
+
+
+        if values['-SIM2-']:
+            possibilidades.append(numeros)
+
+        for q in range(qtd):
+            tipo = choice(possibilidades)
+            caracter = choice(tipo)
+            senha_lista.append(caracter)
+
+        senha_final = ''.join(senha_lista)
+        window['-OUTPUT-'].update(senha_final)
+
+    if event == '-COPIAR-':
+        try:
+            copy(senha_final)
+            sg.popup('Senha copiada!', background_color='Green', font='Arial, 12', text_color='White',
+                     button_type=5, title='')
+        except:
+            sg.popup('Não há senha gerada!', background_color='Orange', font='Arial, 12', text_color='Black',
+                     button_type=5, title='')
+
+
+window.close()
+
